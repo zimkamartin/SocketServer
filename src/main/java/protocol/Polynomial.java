b46d293a8229cbac376d52359fbaf94aab737788
zimkamartin <martin.zimka@gmail.com>
2025-08-24 18:29:20 +0200

    ADD: Class Polynomial (copy from SocketClient).

--- src/main/java/protocol/Polynomial.java
+++ src/main/java/protocol/Polynomial.java
@@ -2,20 +2,34 @@ package protocol;
 
 import java.math.BigInteger;
 
-public class Polynomial {
+/**
+ * Represents a polynomial modulo (X^N + 1) with all coefficients modulo Q.
+ * <p>
+ * These polynomials are basic building blocks in the protocol.
+ * </p>
+ */
+class Polynomial {
 
+    /**
+     * From the lowest to highest position in array, constant to X^(N-1) coefficients are stored.
+     */
     private final BigInteger[] coefficients;
 
     Polynomial(BigInteger[] coefficients) {
         this.coefficients = coefficients;
     }
 
-    public void setCoeffIndex(int i, BigInteger val)
+    void setCoeffIndex(int i, BigInteger val)
     {
         this.coefficients[i] = val;
     }
 
-    public BigInteger[] getCoeffs()
+    BigInteger getCoeffIndex(int i)
+    {
+        return this.coefficients[i];
+    }
+
+    BigInteger[] getCoeffs()
     {
         return this.coefficients;
     }
